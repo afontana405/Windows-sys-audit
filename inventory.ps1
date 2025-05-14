db4e539c27e02da2b800d4b28f824ea2565272d7
afontana405 <alex13579@icloud.com>
2025-05-14 15:56:24 -0500

    audit now stores hardware specs related to CPU, Memory, and Disk

--- inventory.ps1
+++ inventory.ps1
@@ -1,6 +1,18 @@
 $output_dir = "$HOME\Documents\software_inventory"
 mkdir "$output_dir"
 
-# Computer Info
+# Computer
 Write-Output "[+] Collecting Computer Info..."
 Get-ComputerInfo | Out-File "$output_dir\ComputerInfo.txt"
+
+# CPU
+Write-Output "[+] Collecting CPU Info..."
+Get-CimInstance -ClassName Win32_Processor | Out-File "$output_dir\CPU.txt"
+
+# Memory
+Write-Output "[+] Collection Memory Info..."
+Get-CimInstance -ClassName Win32_PhysicalMemory | Out-File "$output_dir\Memory.txt"
+
+# Disk
+Write-Output "[+] Collection Disk Info..."
+Get-CimInstance -ClassName Win32_DiskDrive | Out-File "$output_dir\Disk.txt"
